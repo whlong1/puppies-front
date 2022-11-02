fd32208ffaeaecc9fc2eefb6827420c311c2cb3d
whlong1 <whunterlong@gmail.com>
2022-11-01 23:19:19 -0400

    Stub up Nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
+
+import Nav from './components/Nav'
 import Landing from './pages/Landing'
 
 const App = () => {
+  const [puppies, setPuppies] = useState([])
+  
   return (
     <>
-      <Landing/>
+      <Nav />
+      <Landing />
     </>
   )
 }
