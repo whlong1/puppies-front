f1181b231c72014b460221d1751cb89c68ff1734
whlong1 <whunterlong@gmail.com>
2022-11-03 13:51:03 -0400

    Add initial layout styling to index.css

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,12 @@
 import { NavLink } from "react-router-dom"
+import Logo from '../assets/logo.svg'
 
 const Nav = () => {
   return (
     <nav>
+      <NavLink to="/">
+        <img src={Logo} alt="A cute puppy" />
+      </NavLink>
       <NavLink to="/puppies">Puppy List</NavLink>
       <NavLink to="/puppies/new">New Puppy</NavLink>
     </nav>
