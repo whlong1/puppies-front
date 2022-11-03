90d88d9c27f7dd64c938eb3183dda755c27ad8c1
whlong1 <whunterlong@gmail.com>
2022-11-03 12:40:33 -0400

    Build out basic NavLinks, wrap App in BrowserRouter

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,10 @@
-
+import { NavLink } from "react-router-dom"
 
 const Nav = () => {
   return (
     <nav>
-      
+      <NavLink to="/puppies">Puppy List</NavLink>
+      <NavLink to="/puppies/new">New Puppy</NavLink>
     </nav>
   )
 }
