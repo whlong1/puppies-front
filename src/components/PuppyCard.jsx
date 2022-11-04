c9b204f34b47039d244a186abfa79180aa96c18c
whlong1 <whunterlong@gmail.com>
2022-11-03 23:27:29 -0400

    Build out svg lookup object

--- src/components/PuppyCard.jsx
+++ src/components/PuppyCard.jsx
@@ -1,3 +1,35 @@
+import BlackBat from '../assets/icons/blackBat.svg'
+import BlackDrop from '../assets/icons/blackDrop.svg'
+import BlackButton from '../assets/icons/blackButton.svg'
+
+import GoldenBat from '../assets/icons/goldenBat.svg'
+import GoldenDrop from '../assets/icons/goldenDrop.svg'
+import GoldenButton from '../assets/icons/goldenButton.svg'
+
+import BrownBat from '../assets/icons/brownBat.svg'
+import BrownDrop from '../assets/icons/brownDrop.svg'
+import BrownButton from '../assets/icons/brownButton.svg'
+
+import GreyBat from '../assets/icons/greyBat.svg'
+import GreyDrop from '../assets/icons/greyDrop.svg'
+import GreyButton from '../assets/icons/greyButton.svg'
+
+import WhiteBat from '../assets/icons/whiteBat.svg'
+import WhiteDrop from '../assets/icons/whiteDrop.svg'
+import WhiteButton from '../assets/icons/whiteButton.svg'
+
+
+const obj = {
+  grey: { bat: GreyBat, drop: GreyDrop, button: GreyButton },
+  black: { bat: BlackBat, drop: BlackDrop, button: BlackButton },
+  white: { bat: WhiteBat, drop: WhiteDrop, button: WhiteButton },
+  brown: { bat: BrownBat, drop: BrownDrop, button: BrownButton },
+  golden: { bat: GoldenBat, drop: GoldenDrop, button: GoldenButton },
+}
+
+
+
+
 const PuppyCard = () => {
   return (
     <li></li>
