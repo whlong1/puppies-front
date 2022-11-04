94256f810bf621898e5c65d31840e787ec940831
whlong1 <whunterlong@gmail.com>
2022-11-04 14:24:53 -0400

    Update h1 fontsize and margin

--- src/components/PuppyCard.jsx
+++ src/components/PuppyCard.jsx
@@ -1,19 +1,15 @@
 import GreyBat from '../assets/icons/greyBat.svg'
 import GreyDrop from '../assets/icons/greyDrop.svg'
 import GreyButton from '../assets/icons/greyButton.svg'
-
 import BlackBat from '../assets/icons/blackBat.svg'
 import BlackDrop from '../assets/icons/blackDrop.svg'
 import BlackButton from '../assets/icons/blackButton.svg'
-
 import WhiteBat from '../assets/icons/whiteBat.svg'
 import WhiteDrop from '../assets/icons/whiteDrop.svg'
 import WhiteButton from '../assets/icons/whiteButton.svg'
-
 import BrownBat from '../assets/icons/brownBat.svg'
 import BrownDrop from '../assets/icons/brownDrop.svg'
 import BrownButton from '../assets/icons/brownButton.svg'
-
 import GoldenBat from '../assets/icons/goldenBat.svg'
 import GoldenDrop from '../assets/icons/goldenDrop.svg'
 import GoldenButton from '../assets/icons/goldenButton.svg'
