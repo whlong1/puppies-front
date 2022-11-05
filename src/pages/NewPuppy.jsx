2579f71895081d3694338609193493df2d6a9db7
whlong1 <whunterlong@gmail.com>
2022-11-05 00:37:02 -0400

    Build out initial NewPuppy form

--- src/pages/NewPuppy.jsx
+++ src/pages/NewPuppy.jsx
@@ -1,7 +1,88 @@
+import { useState } from "react"
 const NewPuppy = () => {
+  const [form, setForm] = useState({
+    name: '',
+    breed: '',
+    age: '',
+    color: 'Grey',
+    ears: 'Drop'
+  })
+
+  const handleChange = ({ target }) => {
+    setForm({ ...form, [target.name]: target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('Form Data', form)
+  }
+
   return (
     <main className="new">
       <h1>NEW PUPPY</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name-input">Name</label>
+        <input
+          required
+          type="text"
+          name="name"
+          id="name-input"
+          value={form.name}
+          placeholder="Name"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="title-input">Age</label>
+        <input
+          required
+          type="number"
+          name="age"
+          id="age-input"
+          value={form.age}
+          placeholder="Age"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="breed-input">Breed</label>
+        <input
+          required
+          type="text"
+          name="breed"
+          id="breed-input"
+          value={form.breed}
+          placeholder="Breed"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="color-input">Color</label>
+        <select
+          required
+          name="color"
+          id="color-input"
+          value={form.color}
+          onChange={handleChange}
+        >
+          <option value="Grey">Grey</option>
+          <option value="Black">Black</option>
+          <option value="Brown">Brown</option>
+          <option value="White">White</option>
+          <option value="Golden">Golden</option>
+        </select>
+
+        <label htmlFor="ears-input">Ears</label>
+        <select
+          required
+          name="ears"
+          id="ears-input"
+          value={form.ears}
+          onChange={handleChange}
+        >
+          <option value="Drop">Drop</option>
+          <option value="Bat">Bat</option>
+          <option value="Button">Button</option>
+        </select>
+        <button type="submit">SUBMIT</button>
+      </form>
     </main>
   )
 }
