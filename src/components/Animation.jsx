ae14cbfa783151eb6d46906e90903552defe4557
whlong1 <whunterlong@gmail.com>
2022-11-16 01:02:46 -0500

    Clean up RiveComponent

--- src/components/Animation.jsx
+++ src/components/Animation.jsx
@@ -1,15 +1,11 @@
 import PuppyLogo from '../assets/animations/puppy-logo.riv'
-import { useRive, Layout, Fit, Alignment } from '@rive-app/react-canvas'
+import { useRive } from '@rive-app/react-canvas'
 
 const Animation = () => {
   const { RiveComponent } = useRive({
     src: PuppyLogo,
     autoplay: true,
     stateMachines: "State Machine 1",
-    layout: new Layout({
-      fit: Fit.Cover,
-      alignment: Alignment.TopCenter,
-    }),
   })
 
   return (
