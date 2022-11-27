b80f269cad4211f71b142ee0357d782f3e213209
whlong1 <whunterlong@gmail.com>
2022-11-26 20:33:18 -0500

    Build out pagination buttons, add onClick event handlers

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,6 +1,9 @@
 const Pagination = ({ handlePagination }) => {
   return (
-    <div></div>
+    <div>
+      <button onClick={() => handlePagination(-1)}>Back</button>
+      <button onClick={() => handlePagination(1)}>Next</button>
+    </div>
   )
 }
 
